6d2321b7da552a23a2f25c2bae940e5c9aaae88f
juun-S <jjuny.055@gmail.com>
2024-05-30 18:12:21 +0900

    #25 SecurityConfig기능 설명 주석 추가

--- Hotgirl/src/main/java/kr/bit/hotgirl/component/CustomAuthenticationFailureHandler.java
+++ Hotgirl/src/main/java/kr/bit/hotgirl/component/CustomAuthenticationFailureHandler.java
@@ -9,21 +9,18 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 
 import java.io.IOException;
 
+@Slf4j // Lombok의 로그 관련 어노테이션. 이 어노테이션을 통해 로그를 기록할 수 있다.
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler { // 인증 실패 시 동작할 핸들러 클래스
 
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error(exception.getMessage()); // 에러 메시지 기록
+        log.info("로그인 실패"); // 로그인 실패 정보 기록
+        System.out.println("로그인 실패"); // 콘솔에 로그인 실패 출력
+        response.sendRedirect(request.getContextPath() + "/login?error"); // 로그인 실패 시, 로그인 페이지로 리다이렉트
+        System.out.println(request.getParameter("userId")); // 콘솔에 로그인 시도한 사용자 ID 출력
 
-    @Slf4j
-    public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
-
-        @Override
-        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-          log.error(exception.getMessage());
-          log.info("로그인 실패");
-          System.out.println("로그인 실패");
-          response.sendRedirect(request.getContextPath() + "/login?error");
-            System.out.println(request.getParameter("userId"));
-
-
-            // 여기에 추가적인 로그 기록 코드를 작성할 수 있습니다.
-        }
+        // 여기에 추가적인 로그 기록 코드를 작성할 수 있습니다.
     }
+}
 
--- Hotgirl/src/main/java/kr/bit/hotgirl/config/SecurityConfig.java
+++ Hotgirl/src/main/java/kr/bit/hotgirl/config/SecurityConfig.java
@@ -15,68 +15,69 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-@Configuration
-@EnableWebSecurity
+@Configuration // 스프링 설정 파일임을 명시하는 어노테이션
+@EnableWebSecurity // 웹 보안 기능을 활성화하는 어노테이션
 public class SecurityConfig {
 
-    @Autowired
+    @Autowired // 클래스에 필요한 의존성을 자동으로 주입하는 어노테이션
     private UserRepository userRepository;
 
-    @Bean
+    @Bean // 스프링 빈 객체를 생성하는 어노테이션
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        // 특정 경로에 대한 권한 설정. permitAll()이 통과되면 모든 권한을 가진다.
                         .requestMatchers("/", "/main", "/User/register", "/User/login", "/logout","/maps/maptest",
                                 "/User/check-duplication","/User/login?error", "/User/login?logout", "/User/", "/User/mypage",
                                 "/resources/**","/resources/static/**", "/maps/maptest11", "maps/maptest22").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().authenticated() // 모든 요청에 대해 인증된 사용자만 접근 가능
                 )
                 .formLogin(formLogin -> formLogin
-                        .loginPage("/User/login")
-                        .loginProcessingUrl("/login")
-                        .usernameParameter("userId")
-                        .passwordParameter("userPw")
-                        .defaultSuccessUrl("/", true)
-                        .failureUrl("/User/login?error")
-                        .failureHandler(customAuthenticationFailureHandler())
-                        .permitAll()
+                        .loginPage("/User/login") // 로그인 페이지 경로 설정
+                        .loginProcessingUrl("/login") // 로그인 처리 URL 설정
+                        .usernameParameter("userId") // 사용자명 파라미터 설정
+                        .passwordParameter("userPw") // 비밀번호 파라미터 설정
+                        .defaultSuccessUrl("/", true) // 로그인 성공 시 리다이렉트할 URL 설정
+                        .failureUrl("/User/login?error") // 로그인 실패 시 리다이렉트할 URL 설정
+                        .failureHandler(customAuthenticationFailureHandler()) // 로그인 실패 시 동작할 핸들러 설정
+                        .permitAll() // 모든 권한을 가진 사용자에게 접근 허용
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/User/login?logout")
-                        .permitAll()
+                        .logoutUrl("/logout") // 로그아웃 URL 설정
+                        .logoutSuccessUrl("/User/login?logout") // 로그아웃 성공 시 리다이렉트할 URL 설정
+                        .permitAll() // 모든 권한을 가진 사용자에게 접근 허용
                 )
                 .csrf(csrf -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // CSRF 토큰 저장소 설정
                 )
                 .exceptionHandling(exceptions -> exceptions
-                        .accessDeniedPage("/access-denied")
+                        .accessDeniedPage("/access-denied") // 접근 권한이 없을 때 리다이렉트할 URL 설정
                 )
-                .authenticationProvider(authenticationProvider());
+                .authenticationProvider(authenticationProvider()); // 인증 제공자 설정
 
-        return http.build();
+        return http.build(); // 설정된 HttpSecurity 객체를 반환
     }
 
-    @Bean
+    @Bean // 스프링 빈 객체를 생성하는 어노테이션
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService()); // UserDetailsService 설정
-        provider.setPasswordEncoder(passwordEncoder()); // PasswordEncoder 설정
-        return provider;
+        provider.setUserDetailsService(userDetailsService()); // 사용자 정보 서비스 설정
+        provider.setPasswordEncoder(passwordEncoder()); // 비밀번호 암호화 설정
+        return provider; // 설정된 DaoAuthenticationProvider 객체 반환
     }
 
-    @Bean
+    @Bean // 스프링 빈 객체를 생성하는 어노테이션
     public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService(userRepository);
+        return new CustomUserDetailsService(userRepository); // 사용자 정보 서비스 반환
     }
 
-    @Bean
+    @Bean // 스프링 빈 객체를 생성하는 어노테이션
     public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // BCrypt 암호화 객체 반환
     }
 
-    @Bean
+    @Bean // 스프링 빈 객체를 생성하는 어노테이션
     public CustomAuthenticationFailureHandler customAuthenticationFailureHandler() {
-        return new CustomAuthenticationFailureHandler();
+        return new CustomAuthenticationFailureHandler(); // 커스텀 인증 실패 핸들러 반환
     }
 }
